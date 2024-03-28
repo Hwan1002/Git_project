d2cd4a415ecececc54d0df2dc2d8dee2c6c9646a
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2024-03-28 19:12:14 +0900

    Add files via upload

--- JS/JS.js
+++ JS/JS.js
@@ -1,20 +1,26 @@
+//첫번째 객관식 답가져오기
 function quizAnswer1(){
-    debugger;
     let answer = document.querySelector('.select1 input[name="answer1"]:checked');
     return answer;
 }
+//두번째 객관식 답가져오기
 function quizAnswer2(){
-    debugger;
     let answer = document.querySelector('.select2 input[name="answer2"]:checked');
     return answer;
 }
+//세번째 다중 객관식 답가져오기
 function quizAnswer3(){
-    debugger;
     let answer = document.querySelectorAll('.select3 input[type="radio"]:checked');
     return answer;
 }
-
+//여러개의 텍스트 값 가져오기
+function quizAnswer4(){
+    let answer = document. querySelectorAll('.textContents input[name="txt"]');
+    console.log(answer);
+}
 quizAnswer1();
 quizAnswer2();
 quizAnswer3();
+quizAnswer4();
+
 
