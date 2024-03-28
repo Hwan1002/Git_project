fc9e11ed3df7fcb57643072da6d9d2a39f16f7fb
Hwan1002 <164858936+Hwan1002@users.noreply.github.com>
2024-03-28 15:26:22 +0900

    Add files via upload

--- JS/JS.js
+++ JS/JS.js
@@ -1,5 +1,20 @@
-function quizAnswer(){
-    let answer = document.querySelector('input[name="choice"]:checked');
+function quizAnswer1(){
+    debugger;
+    let answer = document.querySelector('.select1 input[name="answer1"]:checked');
     return answer;
 }
-quizAnswer();
+function quizAnswer2(){
+    debugger;
+    let answer = document.querySelector('.select2 input[name="answer2"]:checked');
+    return answer;
+}
+function quizAnswer3(){
+    debugger;
+    let answer = document.querySelector('.select3 input[type="radio"]:checked');
+    return answer;
+}
+
+quizAnswer1();
+quizAnswer2();
+quizAnswer3();
+
